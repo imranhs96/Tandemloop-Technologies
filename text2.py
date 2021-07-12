1027c77449e47749c8812b1d15bd0206043cd492
imranhs96 <85359080+imranhs96@users.noreply.github.com>
2021-07-12 16:34:44 +0530

    Create text2.py

---
+++ text2.py
@@ -0,0 +1,13 @@
+#python
+n=int(input('Enter the value   :'))
+j=1
+if n%2 !=0:
+    for i in range(1,n+1):
+        print(j,end=' ')
+        j+=2
+   
+else:
+    for i in range(1,n+1):
+        print(j,end=' ')
+        j+=2
+    
